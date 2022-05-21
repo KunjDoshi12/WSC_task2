8699fa0377e66b29056a87bbb8a0302312437490
KunjDoshi12 <99990304+KunjDoshi12@users.noreply.github.com>
2022-05-21 18:35:01 +0530

    Add files via upload

--- Q2b.c
+++ Q2b.c
@@ -2,13 +2,9 @@
 
 int main()
 {
-   float p,n,a;
-   //Case 1, first draw to get an ace
-   n=13;
+   float a;
 
-   //Case 2,2nd draw to get a jack
-   p=(4/51);
-   a = (51*14)/4;
+   a=(765/8);
    printf("The number of of cards that need to be drawn to get an Ace and and a Jack is %.0f",a);
 return 0;
 }
